be6ed427725e10a5bed8bbc314a1cb35be0c8b24
yagmurozcaan <149259566+yagmurozcaan@users.noreply.github.com>
2025-04-27 15:25:18 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 # database.py
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/GYK2-Northwind"
+DATABASE_URL = "postgresql://postgres:******s@localhost:5432/GYK2-Northwind"
 engine = create_engine(DATABASE_URL)
